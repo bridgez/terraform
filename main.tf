9632eb8c815a6055a5cde6f27e372bc33a5c9dee
bridgez <efool@qq.com>
2023-11-04 18:49:08 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -38,3 +38,7 @@ variable "AWS_ACCESS_KEY_ID" {
 variable "AWS_SECRET_ACCESS_KEY" {
   description = "AWS Secret Access Key"
 }
+
+variable "AWS_DEFAULT_REGION" {
+  description = "AWS_DEFAULT_REGION"
+}
