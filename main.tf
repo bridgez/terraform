b95b1de3a856330232611041c9d3f9ba2028b972
bridgez <efool@qq.com>
2023-11-04 18:38:43 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -31,11 +31,10 @@ resource "aws_instance" "app_server" {
   }
 }
 
-# variable "AWS_ACCESS_KEY_ID" {
-#   description = "AWS Access Key ID"
-# }
+variable "AWS_ACCESS_KEY_ID" {
+  description = "AWS Access Key ID"
+}
 
-# variable "AWS_SECRET_ACCESS_KEY" {
-#   description = "AWS Secret Access Key"
-# }
-
+variable "AWS_SECRET_ACCESS_KEY" {
+  description = "AWS Secret Access Key"
+}
