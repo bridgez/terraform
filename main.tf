d176c84eb05cec4813d0041d00142ec9f0c3ed60
bridgez <efool@qq.com>
2023-11-04 16:29:11 +0800

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,81 @@
+
+# terraform { cloud { organization = "bridgez" workspaces { name = "example-workspace" } } }
+terraform {
+  cloud {
+    organization = "bridgez"
+    workspaces {
+      name = "example-workspace"      
+    }
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "us-west-2"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-830c94e3"
+  instance_type = "t2.micro"
+
+  tags = {
+    # Name = "ExampleAppServerInstance"
+    Name = var.instance_name
+  }
+}
+
+# variable "AWS_ACCESS_KEY_ID" {
+#   description = "AWS Access Key ID"
+# }
+
+# variable "AWS_SECRET_ACCESS_KEY" {
+#   description = "AWS Secret Access Key"
+# }
+# terraform { cloud { organization = "bridgez" workspaces { name = "example-workspace" } } }
+terraform {
+  cloud {
+    organization = "bridgez"
+    workspaces {
+      name = "example-workspace"      
+    }
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "us-west-2"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-830c94e3"
+  instance_type = "t2.micro"
+
+  tags = {
+    # Name = "ExampleAppServerInstance"
+    Name = var.instance_name
+  }
+}
+
+# variable "AWS_ACCESS_KEY_ID" {
+#   description = "AWS Access Key ID"
+# }
+
+# variable "AWS_SECRET_ACCESS_KEY" {
+#   description = "AWS Secret Access Key"
+# }
